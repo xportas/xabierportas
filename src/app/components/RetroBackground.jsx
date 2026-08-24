09601e15a0badbcff0009a6fb3bcf3aa227bf7af
xportas <xabierportasglz@gmail.com>
2026-08-24 21:01:25 +0200

    refact (retro screen background): change background

---
+++ src/app/components/RetroBackground.jsx
@@ -0,0 +1,12 @@
+'use client';
+import styles from './RetroBackground.module.css';
+
+export default function RetroBackground() {
+  return (
+    <div
+      className={styles.retroBackground}
+      aria-hidden="true"
+      id="retro-outrun-bg"
+    />
+  );
+}
